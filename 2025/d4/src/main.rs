f52c1e10ed8d61e2b93b6d0734913bcfecab7e45
Crimcode <valentin@vimunteanu.me>
2025-12-04 15:59:53 +0200

    2025_D4 optimized

--- 2025/d4/src/main.rs
+++ 2025/d4/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashSet, VecDeque},
+    collections::VecDeque,
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
@@ -16,9 +16,9 @@ const DIRECTIONS: [(isize, isize); 8] = [
     (1, 1),
 ];
 
-fn generate_counts(input: &[Vec<char>]) -> Vec<Vec<i8>> {
-    let n = input[0].len();
-    let mut roll_count: Vec<Vec<i8>> = vec![vec![8; n]; n];
+fn generate_counts(input: &[Vec<char>]) -> Vec<Vec<i64>> {
+    let n = input.len();
+    let mut roll_count: Vec<Vec<i64>> = vec![vec![8; n]; n];
     (1..n - 1).for_each(|i| {
         roll_count[0][i] = 5;
         roll_count[i][n - 1] = 5;
@@ -32,14 +32,12 @@ fn generate_counts(input: &[Vec<char>]) -> Vec<Vec<i8>> {
     roll_count
 }
 
-struct FirstRunResult(VecDeque<(usize, usize)>, HashSet<(usize, usize)>);
-
-fn first_run(input: &[Vec<char>], roll_count: &mut [Vec<i8>]) -> FirstRunResult {
-    let mut checked = HashSet::new();
+fn first_run(input: &mut [Vec<char>], roll_count: &mut [Vec<i64>]) -> VecDeque<(usize, usize)> {
+    let n = input.len();
     let mut to_mark = VecDeque::new();
-    input.iter().enumerate().for_each(|(i, row)| {
-        row.iter().enumerate().for_each(|(j, &c)| {
-            if c == '.' {
+    for i in 0..n {
+        for j in 0..n {
+            if input[i][j] == '.' {
                 DIRECTIONS.iter().for_each(|&(i_change, j_change)| {
                     let (Some(nb_i), Some(nb_j)) = (
                         i.checked_add_signed(i_change),
@@ -47,32 +45,30 @@ fn first_run(input: &[Vec<char>], roll_count: &mut [Vec<i8>]) -> FirstRunResult 
                     ) else {
                         return;
                     };
-                    if !(nb_i < input.len() && nb_j < input[0].len()) {
+                    if !(nb_i < n && nb_j < n) {
                         return;
                     }
                     roll_count[nb_i][nb_j] -= 1;
-                    if input[nb_i][nb_j] == '@'
-                        && roll_count[nb_i][nb_j] < 4
-                        && checked.insert((nb_i, nb_j))
-                    {
+                    if input[nb_i][nb_j] == '@' && roll_count[nb_i][nb_j] < 4 {
                         to_mark.push_back((nb_i, nb_j));
+                        input[nb_i][nb_j] = 'x';
                     }
                 })
             }
-        })
-    });
-    FirstRunResult(to_mark, checked)
+        }
+    }
+    to_mark
 }
 
-fn solve_part_1(input: &[Vec<char>]) -> usize {
+fn solve_part_1(input: &mut [Vec<char>]) -> usize {
     let mut roll_count = generate_counts(input);
-    let FirstRunResult(to_mark, _) = first_run(input, &mut roll_count);
+    let to_mark = first_run(input, &mut roll_count);
     to_mark.len()
 }
 
 fn solve_part_2(input: &mut [Vec<char>]) -> usize {
     let mut roll_count = generate_counts(input);
-    let FirstRunResult(mut to_mark, mut checked) = first_run(input, &mut roll_count);
+    let mut to_mark = first_run(input, &mut roll_count);
     let mut answer = to_mark.len();
     while let Some((i, j)) = to_mark.pop_front() {
         input[i][j] = 'x';
@@ -89,9 +85,9 @@ fn solve_part_2(input: &mut [Vec<char>]) -> usize {
             roll_count[nb_i][nb_j] -= 1;
             if input[nb_i][nb_j] == '@'
                 && roll_count[nb_i][nb_j] < 4
-                && checked.insert((nb_i, nb_j))
             {
                 to_mark.push_back((nb_i, nb_j));
+                input[nb_i][nb_j] = 'x';
                 answer += 1;
             }
         })
@@ -112,7 +108,7 @@ fn main() {
 
             println!(
                 "{filename}\npart 1: {}\npart 2: {}\n",
-                solve_part_1(&input),
+                solve_part_1(&mut input.clone()),
                 solve_part_2(&mut input)
             )
         });
